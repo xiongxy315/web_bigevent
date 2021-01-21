7025824c3dba3b8bbad4d2dc5342ff6f8a1d1ed2
xiongxy315 <1255173519@qq.com>
2021-01-21 11:19:51 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,9 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+//每次调用ajax请求时，会先调用这个函数
+//在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (options) {
+    console.log(options); //可以拿到我点击的任意按钮请求的所有信息
+    //拼接上根路径
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+    //注意这个文件的引入要在jq之后我们的js之前
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,46 +1,52 @@
-$(function() {
+$(function () {
+  //1.因为登录和注册页面很相似，改变按钮，弹出对应的框就可以了
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide(); //登录页面隐藏
+    $('.reg-box').show(); //注册页面显示
+  })
+  //点击“去登录”的链接
+  $('#link_login').on('click', function () {
+    $('.login-box').show(); //登录页面显示
+    $('.reg-box').hide(); //注册页面隐藏
   })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
 
-  // 从 layui 中获取 form 对象
+  //2.自定义校验规则（从layui中获取form对象
+  //导入了layui脚本就有个layui
   var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  //通过form.verify()函数自定义校验规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
+    //自定义了一个叫做pwd的校验规则
+    pwd: [
+      /^[\S]{6,12}$/,
+      '密码必须6到12位，且不能出现空格'
+    ],
+    username: [
+      /^[a-zA-Z0-9_-]{3,16}$/,
+      '用户名必须3-16位，不能包括除_-以外的特殊符号'
+    ],
+    //校验两次密码是否一致的规则
+    repwd: function (value) {
+      //通过形参拿到的是确认密码框中的内容
+      //还需要拿到密码框中的内容
+      //然后进行一次等于的判断
+      //如果判断失败，，则return一个提示消息即可
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
       }
-    }
-  })
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
+
+  });
+  // 3.监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
+    //  阻止默认的提交行为
+    e.preventDefault()
+    //  发起Ajax的POST请求
+    var data = $(this).serialize();
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -50,8 +56,8 @@ $(function() {
     })
   })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
+  // 4.监听登录表单的提交事件
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
@@ -59,7 +65,7 @@ $(function() {
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
@@ -67,7 +73,7 @@ $(function() {
         // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
-        location.href = '/index.html'
+        location.href = '/index.html';
       }
     })
   })
